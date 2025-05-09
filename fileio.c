65d66a7d8d76f6a8397121bf52b24cc796e70476
jian diao <jiandiao@agenewtech.com>
2025-05-09 10:15:16 +0800

    第二笔提交：拓展功能以写入指定内容

--- fileio.c
+++ fileio.c
@@ -4,7 +4,6 @@
 int main() {
 
     FILE *fp;
-
     // 打开文件用于写入。如果文件不存在，则会自动创建。
     fp = fopen("test.txt", "w");
 
@@ -13,12 +12,14 @@ int main() {
         printf("无法创建文件！\n");
         return 1;
     }
+    // 写入文件的字符串
+    const char *content = "这是准备要写入文件的指定内容。";
+
+    // 写入内容到文件
+    fprintf(fp, "%s", content);
 
     // 文件操作成功的信息输出
-    printf("文件已创建。\n");
-
-    // 关闭文件
+    printf("文件已创建，并写入了指定内容。\n");
     fclose(fp);
-
     return 0;
 }
