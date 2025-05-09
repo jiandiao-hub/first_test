e45469d474864c1588ca230b5b5d81f438302dac
jian diao <jiandiao@agenewtech.com>
2025-05-09 10:07:38 +0800

    初始提交：创建文件

---
+++ fileio.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    FILE *fp;
+
+    // 打开文件用于写入。如果文件不存在，则会自动创建。
+    fp = fopen("test.txt", "w");
+
+    // 检查文件是否成功打开
+    if (fp == NULL) {
+        printf("无法创建文件！\n");
+        return 1;
+    }
+
+    // 文件操作成功的信息输出
+    printf("文件已创建。\n");
+
+    // 关闭文件
+    fclose(fp);
+
+    return 0;
+}
